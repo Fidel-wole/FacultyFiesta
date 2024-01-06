abc6f9726a6099cdc53c350c485da8e5aa1b5d3a
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2024-01-06 20:32:10 +0100

    add birthday  backend

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const {connectDB} = require("./src/db/conn");
+const router = require("./src/routes/celebrants")
+app.use(cors());
+app.use(bodyparser.json())
+
+connectDB();
+app.use(router);
+app.listen(8000, (req, res)=>{
+    console.log("Server listening at port 8000")
+})
